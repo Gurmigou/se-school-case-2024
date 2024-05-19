bd7e22c7040d13bc0acd19c14d5c7399aa54659f
YehorBukhane <yehor.bukhanets@software-aspekte.de>
2024-05-19 13:02:52 +0300

    [End-points]: Added end-points

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"se-school-case/controller"
+	"se-school-case/initializer"
+	"se-school-case/service"
+)
 
+func init() {
+	initializer.LoadEnvVariables()
+	initializer.ConnectToDatabase()
+	initializer.AutoMigrateDatabase()
+	service.StartRateUpdater()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/api/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+	r.POST("/api/subscribe", controller.PostAddUserEmail)
+	r.GET("/api/rate", controller.GetExchangeRate)
+
+	initializer.StartServer(r)
 }
